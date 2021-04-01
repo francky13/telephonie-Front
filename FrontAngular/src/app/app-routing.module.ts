6419de222739b3d48454c1cd2a3a730fac9a5d16
francky13 <rabemiakatrafrancky@gmail.com>
2021-04-01 17:57:46 +0300

    vio le izy

--- FrontAngular/src/app/app-routing.module.ts
+++ FrontAngular/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HistoriqueComponent } from './historique/historique.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'Login', component : LoginComponent},
+  {path: 'Historique', component : HistoriqueComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- FrontAngular/src/app/app.module.ts
+++ FrontAngular/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HistoriqueComponent } from './historique/historique.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    HistoriqueComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
